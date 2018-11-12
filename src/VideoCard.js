fdaf4eb04a25fa99ddfd649ed5e73668c60fcf68
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-11-11 23:35:29 -0500

    feat: Identify DVD vs Blue-ray

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,4 +1,4 @@
-import {Card, CardBody, CardFooter, CardImage, CardText, CardTitle, Col, Tooltip} from "mdbreact";
+import {Card, CardBody, CardFooter, CardImage, CardText, CardTitle, Col, Tooltip, Badge} from "mdbreact";
 import React, {Component} from 'react';
 
 class VideoCard extends Component {
@@ -12,23 +12,24 @@ class VideoCard extends Component {
           <CardImage cascade src={video.Poster} top alt={video.Title}/>
           <CardBody cascade className="text-center">
             <a href="" className="text-muted">
-              <h5>{video.Title}</h5>
+              <h5 color="unique-color-dark">{video.Title}</h5>
             </a>
             <CardTitle>
               <strong>{video.Director}</strong>
             </CardTitle>
             <CardText>{video.Plot}</CardText>
             <CardFooter className="px-1">
-                    <span className="float-left font-weight-bold">
-                      <strong>${video.price}</strong>
-                    </span>
+              <span className="float-left font-weight-bold">
+                <strong>${video.price}</strong>
+              </span>
               <span className="float-right">
-                      <a onClick={() => this.props.onCartClick()}><Tooltip placement="top" tag="a" component="i"
-                                                                   componentClass="fa fa-shopping-cart grey-text ml-3"
-                                                                   tooltipContent="Add to Cart"/></a>
-                      <a onClick={() => this.props.onFavClick()}><Tooltip placement="top" tag="a" component="i"
-                                                            componentClass="fa fa-heart grey-text ml-3"
-                                                            tooltipContent="Add to Watchlist"/></a>
+                <Badge color={video.cdType === 'DVD' ? "green" : "orange"} pill>{video.cdType}</Badge>{'    '}
+                <a onClick={() => this.props.onCartClick()}><Tooltip placement="top" tag="a" component="i"
+                                                                     componentClass="fa fa-shopping-cart grey-text ml-3"
+                                                                     tooltipContent="Add to Cart"/></a>
+                <a onClick={() => this.props.onFavClick()}><Tooltip placement="top" tag="a" component="i"
+                                                                    componentClass="fa fa-heart grey-text ml-3"
+                                                                    tooltipContent="Add to Watchlist"/></a>
               </span>
             </CardFooter>
           </CardBody>

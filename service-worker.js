d8b4e07332f8ec540267dffac5ac95d23b133e89
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-11-12 23:05:20 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/the-pirate-shop/precache-manifest.7b1571d944324fe439d8934daefdade7.js"
+  "/the-pirate-shop/precache-manifest.94d5a4dfe09930a2a783e2f0abe9cc55.js"
 );
 
 workbox.clientsClaim();

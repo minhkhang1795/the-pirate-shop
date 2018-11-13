2ba27d5a8aecabcb7f8087fb00eddf7cff5db848
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2018-11-13 00:56:36 -0500

    Updates

--- precache-manifest.e5236aa91ce024f4d9f40ad206f20941.js
+++ precache-manifest.e5236aa91ce024f4d9f40ad206f20941.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "69f8a0617ac472f78e45841323a3df9e",
-    "url": "/the-pirate-shop/static/media/Roboto-Light.69f8a061.woff2"
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/the-pirate-shop/static/media/Roboto-Thin.94998475.ttf"
   },
   {
-    "revision": "e279d7c8375f7eec7b3b",
-    "url": "/the-pirate-shop/static/css/main.b11af389.chunk.css"
+    "revision": "3acefd5e17f6d62c05a2",
+    "url": "/the-pirate-shop/static/css/main.5d050c24.chunk.css"
   },
   {
     "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
@@ -20,14 +20,6 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/js/runtime~main.019a476c.js"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/the-pirate-shop/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/the-pirate-shop/static/media/fontawesome-webfont.fee66e71.woff"
-  },
-  {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/the-pirate-shop/static/media/fontawesome-webfont.af7ae505.woff2"
   },
@@ -36,36 +28,44 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/the-pirate-shop/static/media/fontawesome-webfont.912ec66d.svg"
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/the-pirate-shop/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "954bbdeb86483e4ffea00c4591530ece",
-    "url": "/the-pirate-shop/static/media/Roboto-Thin.954bbdeb.woff2"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/the-pirate-shop/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/the-pirate-shop/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "dfe56a876d0282555d1e2458e278060f",
     "url": "/the-pirate-shop/static/media/Roboto-Thin.dfe56a87.eot"
   },
   {
-    "revision": "7500519de3d82e33d1587f8042e2afcb",
-    "url": "/the-pirate-shop/static/media/Roboto-Thin.7500519d.woff"
-  },
-  {
     "revision": "a990f611f2305dc12965f186c2ef2690",
     "url": "/the-pirate-shop/static/media/Roboto-Light.a990f611.eot"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/the-pirate-shop/static/media/Roboto-Thin.94998475.ttf"
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/the-pirate-shop/static/media/Roboto-Thin.954bbdeb.woff2"
   },
   {
-    "revision": "3b813c2ae0d04909a33a18d792912ee7",
-    "url": "/the-pirate-shop/static/media/Roboto-Light.3b813c2a.woff"
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/the-pirate-shop/static/media/Roboto-Light.69f8a061.woff2"
   },
   {
-    "revision": "e279d7c8375f7eec7b3b",
-    "url": "/the-pirate-shop/static/js/main.e279d7c8.chunk.js"
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/the-pirate-shop/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/the-pirate-shop/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "3acefd5e17f6d62c05a2",
+    "url": "/the-pirate-shop/static/js/main.3acefd5e.chunk.js"
   },
   {
     "revision": "2751ee43015f9884c3642f103b7f70c9",
@@ -76,8 +76,8 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/media/Roboto-Regular.ba3dcd89.woff"
   },
   {
-    "revision": "30799efa5bf74129468ad4e257551dc3",
-    "url": "/the-pirate-shop/static/media/Roboto-Regular.30799efa.eot"
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/the-pirate-shop/static/media/Roboto-Light.3b813c2a.woff"
   },
   {
     "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
@@ -96,10 +96,6 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/media/Roboto-Medium.574fd0b5.woff2"
   },
   {
-    "revision": "df7b648ce5356ea1ebce435b3459fd60",
-    "url": "/the-pirate-shop/static/media/Roboto-Regular.df7b648c.ttf"
-  },
-  {
     "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
     "url": "/the-pirate-shop/static/media/Roboto-Bold.ecdd509c.eot"
   },
@@ -112,12 +108,16 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/media/Roboto-Bold.dc81817d.woff"
   },
   {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/the-pirate-shop/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
     "revision": "11911410dca2de148f30954eb2fd5eab",
     "url": "/the-pirate-shop/static/media/arrow_left.11911410.svg"
   },
   {
-    "revision": "8ef6a08cdc1154920165680a4edde771",
-    "url": "/the-pirate-shop/static/media/arrow_right.8ef6a08c.svg"
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/the-pirate-shop/static/media/Roboto-Regular.df7b648c.ttf"
   },
   {
     "revision": "894a2ede85a483bf9bedefd4db45cdb9",
@@ -128,7 +128,7 @@ self.__precacheManifest = [
     "url": "/the-pirate-shop/static/css/1.18e584d9.chunk.css"
   },
   {
-    "revision": "0b09d7d66991dbf3dade7566dd3401e2",
+    "revision": "d7e56a3bd18ff1431a295f3a4b62000d",
     "url": "/the-pirate-shop/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/the-pirate-shop/precache-manifest.fb6055385cd10d50e41cbf2a9e369958.js"
+  "/the-pirate-shop/precache-manifest.e5236aa91ce024f4d9f40ad206f20941.js"
 );
 
 workbox.clientsClaim();
